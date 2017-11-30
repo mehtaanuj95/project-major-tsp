8a9c6e7302b4b6160b4c84af47dd7d1417ed44b8
Anuj Bhai Mehta <mehta.anuj2309@gmail.com>
2017-12-01 00:56:38 +0530

    updated the code slightly

--- window_SCX.js
+++ window_SCX.js
@@ -1,11 +1,4 @@
-/*function onlyUnique(value, index, self) { 
-    return self.indexOf(value) === index;
-}
 
-// usage example:
-var a = ['a', 1, 'a', 2, '1'];
-var unique = a.filter( onlyUnique );
-console.log(unique);*/
 //function to return only unique elements
 function onlyUnique(value, index, self) { 
     return self.indexOf(value) === index;
